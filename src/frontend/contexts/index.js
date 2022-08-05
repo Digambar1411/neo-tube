7546bee0d77e6c51ba13dd90dc277d43b5bbcef3
Digambar <deshawaldigambar@gmail.com>
2022-08-05 20:14:32 +0530

    barrel import export

--- src/frontend/contexts/index.js
+++ src/frontend/contexts/index.js
@@ -1,5 +1,7 @@
 import { SidebarProvider ,useSidebar} from "./Sidebar-context";
 import { ThemeProvider, useTheme } from "./theme-context";
+import { CategoryProvider, useCategory } from "./category-context";
+import { VideosProvider, useVideos } from "./videos-context";
 
 
-export{ SidebarProvider,  useSidebar, ThemeProvider, useTheme}
+export{ SidebarProvider,  useSidebar, ThemeProvider, useTheme, VideosProvider, useVideos, CategoryProvider, useCategory}
