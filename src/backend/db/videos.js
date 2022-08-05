fb457ed66c0f951b03996708042c93688938bf01
Digambar <deshawaldigambar@gmail.com>
2022-08-05 20:12:57 +0530

    updated database

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -1,21 +1,237 @@
-/**
- * Videos Database can be added here.
- * You can add videos of your wish with different attributes
- * */
 
-export const videos = [
+ export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: "gcNGaTYwA3I",
+    title: "4 Products in Your Kitchen You Should Throw Away (With Healthier Alternatives)",
+    categoryName:"Health Awareness",
+    views:"83K",
+    channel:"Fit Tuber",
+    channelProfile:"ytc/AMLnZu-e1AZQMfQgV6NMju1CBiycOZON54u7gs1AdRIo=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:"img.youtube.com/vi/gcNGaTYwA3I/maxresdefault.jpg",
+    uploadedAt:new Date("Feb 25, 2022"),
+    description:"In this video, I am going to share with you 4 kitchen products that you throw away immediately. Most of these products are used daily in Indian households and to know that these products pose some serious health hazards is really disturbing. It’s high time we talk about them as I can assure you that most of you would be using 3 out of these 4 products on a daily basis. We will discuss why these products are detrimental and how easily you can switch to healthier alternatives. ",   
   },
+
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    _id: "vqRBCa1Jv4w",
+    title: "5 Natural Toothpastes in India Under Rs 219 that You Must Try (#3 is just Rs 70)",
+    categoryName:"Health Awareness",
+    views:"829K",
+    channel:"Fit Tuber",
+    channelProfile:"ytc/AMLnZu-e1AZQMfQgV6NMju1CBiycOZON54u7gs1AdRIo=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/vqRBCa1Jv4w/hqdefault.jpg",
+    uploadedAt:new Date("Jun 17, 2022"),
+    description:"Brushing your teeth is an indispensable part of personal hygiene. However, it is even more important to choose a toothpaste which is free from harmful chemicals and is also pocket friendly. And if you thought that natural products are always expensive then let us break the myth as in this video I am going to share with you 5 toothpastes which are 100% safe and also under Rs 219. Look out for #3 as it is natural and yet cheaper than your current toothpaste. ",   
   },
+
+  {
+    _id: "rfscVS0vtbw",
+    title: "Learn Python - Full Course for Beginners [Tutorial]",
+    categoryName: "Programming",
+    views:"35M",
+    channel:"freeCodeCamp.org",
+    channelProfile:"ytc/AMLnZu9UWrGceKWaqm8AF89vuxrEt8MO3E59qOoQ785Lew=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/rfscVS0vtbw/hqdefault.jpg",
+    uploadedAt:new Date("Jul 11, 2018"),
+    description:"This course will give you a full introduction into all of the core concepts in python. Follow along with the videos and you'll be a python programmer in no time! Want more from Mike? He's starting a coding RPG/Bootcamp - https://simulator.dev/",   
+  },
+
+  {
+    _id: "PkZNo7MFNFg",
+    title: "Learn JavaScript - Full Course for Beginners",
+    categoryName: "Programming",
+    views:"10M",
+    channel:"freeCodeCamp.org",
+    channelProfile:"ytc/AMLnZu9UWrGceKWaqm8AF89vuxrEt8MO3E59qOoQ785Lew=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/PkZNo7MFNFg/hqdefault.jpg",
+    uploadedAt:new Date("Dec 10, 2020"),
+    description:" This complete 134-part JavaScript tutorial for beginners will teach you everything you need to know to get started with the JavaScript programming language.",   
+  },
+ 
+  {
+    _id: "tz0fDABt67g",
+    title: "Every Beginner React Developer Makes This Mistake With State",
+    categoryName: "Programming",
+    views:"163K",
+    channel:"Web Dev Simplified",
+    channelProfile:"ytc/AMLnZu-PjRc81p0qP-ZPWxv8aY4k-4gFRzGEYs3HWbIQ=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/tz0fDABt67g/hqdefault.jpg",
+    uploadedAt:new Date("Jun 25, 2022"),
+    description:"By far one of the biggest beginner React mistakes I see is developers storing derived state. This is a huge problem that is incredibly hard to detect unless you know what you are looking for. In this video I will show you what the signs are of derived state so you can make sure you never store derived state.",   
+  },
+
+  {
+    _id: "2md4HQNRqJA",
+    title: "Welcome to new JavaScript course",
+    categoryName: "Programming",
+    views:"7.8k",
+    channel:"Hitesh Choudhary",
+    channelProfile:"ytc/AMLnZu--BW5u0yDUi9roKCJ5iW3NSFdvxTToEMOYzXjcyw=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/2md4HQNRqJA/hqdefault.jpg",
+    uploadedAt:new Date("Jul 05, 2022"),
+    description:"Welcome to new JavaScript course.",   
+  },
+
+   {
+    _id: "NkGhs_n1PCg",
+    title: "Creating a Form for Adding Product | NextJs Tutorial for Beginners #78",
+    categoryName: "Programming",
+    views:"8K",
+    channel:"CodeWithHarry",
+    channelProfile:"ytc/AMLnZu8dZQJYCt6Ffcd-pl113huuo_HJ3PpvgkyFk5FkrQ=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/NkGhs_n1PCg/hqdefault.jpg",
+    uploadedAt:new Date("Jun 25, 2022"),
+    description:"Creating a Form for Adding Product | NextJs Tutorial for Beginners #78.",   
+  },
+
+  {
+    _id: "0cSVuySEB0A",
+    title: "React Router v6 in depth guide",
+    categoryName:"Programming",
+    views:"567K",
+    channel:"Hitesh Choudhary",
+    channelProfile:"ytc/AMLnZu--BW5u0yDUi9roKCJ5iW3NSFdvxTToEMOYzXjcyw=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/0cSVuySEB0A/hqdefault.jpg",
+    uploadedAt:new Date("Nov 09, 2021"),
+    description:"Welcome to react-router version 6 in-depth guide. In this video, we will learn about react-router v6 but creating a fresh project. I assume that you have no prior experience with react-router. In case you have, you will learn to upgrade your project from version 5 to version 6.",   
+  },
+
+  {
+    _id: "TGHqBX1YJRc",
+    title: "The Men who Built India | Untold Story of Tata | Dhruv Rathee",
+    category:"Social Awareness",
+    views:"3M",
+    channel:"Dhruv Rathee",
+    channelProfile:"oQ2NQMuq-9IS6_MZdsGXa0RWecaACremA01Z7jo-YpoEF1H6PyUF8PZzXkV10OYwSP3UMJDeTg=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/TGHqBX1YJRc/hqdefault.jpg",
+    uploadedAt:new Date("Jul 04, 2022"),
+    description:"Tata Group is considered one of India's most impactful companies not just in terms of how much it contributes to our country's economy, but also in terms of the values established by Jamsetji Tata which are still in place under the leadership of Ratan Tata. It originated in the era when India was in the hands of the Britishers and Independent Indian companies were not normalized. However, despite such hurdles, the strong vision and strategies led Tata to where it is today, and its story is worth listening to. Watch this video and get on a journey with Dhruv Rathee as he takes you back to the history of Tata, & how it shaped India as a nation.",   
+  },
+
+  {
+    _id: "Iwz_Vdz8EKw",
+    title: "Agneepath Scheme Explained | Good or Bad? | Agniveers | Dhruv Rathee ft.",
+    category:"Social Awareness",
+    views:"5M",
+    channel:"Dhruv Rathee",
+    channelProfile:"oQ2NQMuq-9IS6_MZdsGXa0RWecaACremA01Z7jo-YpoEF1H6PyUF8PZzXkV10OYwSP3UMJDeTg=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/Iwz_Vdz8EKw/hqdefault.jpg",
+    uploadedAt:new Date("Jun 19, 2022"),
+    description:"Agneepath Scheme or Agnipath Scheme 2022 is a new scheme introduced by the government to recruit the youth into the Indian Army for 4 years with a compensation of 30k per month. The recruited people would be known as Agniveers. However, there are some people who are protesting against this initiative. In fact, some of these protests have turned violent too. So what is the reality of this scheme? Are those protests valid? Watch this video to find out as I talk about the Agneepath scheme and the happenings revolving around it in detail.",   
+  },
+
+  {
+    _id: "Wy017TbTzuY",
+    title: "ITR Filing for salaried employees | Online Tutorial AY 2022-23 | Income Tax Return | LLA",
+    category:"Social Awareness",
+    views:"576K",
+    channel:"Labour Law Advisor ",
+    channelProfile:"x1JSvmCagWY4M7yDxuf42ZjSUqY5RhomZsN35VIysRDlfwgbiHF3ygLw0DbA4j46BWxClqs3Gg=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/Wy017TbTzuY/hqdefault.jpg",
+    uploadedAt:new Date("Jul 26, 2022"),
+    description:"This video is a step-by-step guide on how to file an Income tax Return Online for AY 2022-23 for a salaried employee. Who are eligible to file ITR 1? What comes under form 16 and form 26AS? We will also look at some of the tax deductions and exemptions. By the end of this video, I Jagruk RJ shared some of my ninja techniques which will help you file your ITR more efficiently.",   
+  },
+
+  {
+    _id: "l3Acc3D8OH4",
+    title: "[Secrets Revealed] How to find the BEST Health Insurance Policy? ft @B Wealthy",
+    category:"Social Awareness",
+    views:"817K",
+    channel:"Labour Law Advisor ",
+    channelProfile:"x1JSvmCagWY4M7yDxuf42ZjSUqY5RhomZsN35VIysRDlfwgbiHF3ygLw0DbA4j46BWxClqs3Gg=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/l3Acc3D8OH4/hqdefault.jpg",
+    uploadedAt:new Date("Feb 03, 2021"),
+    description:"Here it is, the much-awaited guide on how to find the best health insurance policy for yourself and your family",   
+  },
+
+ 
+
+   {
+    _id: "GKyCVpBh_Zw",
+    title: "जगभरात मंदीची लाट येईल अशी शक्यता असताना भारतात ही मंदी 0% असेल अस का म्हटलं जातयं | Bol Bhidu",
+    category:"News",
+    views:"49K",
+    channel:"Bol Bhidu",
+    channelProfile:"8d6ERqjLo_XCYH-0DWBd9L4tNKEHgZNSGBlZsxpCPRi6BgdcJmGfwmidRBwS7J5gAmE8JWFx=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/GKyCVpBh_Zw/hqdefault.jpg",
+    uploadedAt:new Date("Jul 31, 2022"),
+    description:"जगभरात सध्या मंदीची सुरूवात झालीये अस म्हटलं जातय. अनेक देश मंदीच्या उंबरठ्यावर आहेत. परंतु यामध्ये भारताबाबत जे भाकित केलं जातय त्यात या मंदीची जळ भारताला बसणार नाही अस म्हटलं जातय आणि भारतात ही मंदी 0% असेल अस म्हणतायत. तर हे कस काय? यामागची कारण काय आहेत? ",   
+  },
+
+  {
+    _id: "7qrHperoAMA",
+    title: "आणि नरेंद्र मोदींनी ठरवलं, आता गडकरींना दाखवून द्यायचं आपलं भाजपमध्ये किती वजन आहे.. | BolBhidu #BJP",
+    category:"News",
+    views:"496K",
+    channel:"Bol Bhidu",
+    channelProfile:"8d6ERqjLo_XCYH-0DWBd9L4tNKEHgZNSGBlZsxpCPRi6BgdcJmGfwmidRBwS7J5gAmE8JWFx=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/7qrHperoAMA/hqdefault.jpg",
+    uploadedAt:"Jul 26, 2022",
+    uploadedAt: new Date("May 22, 2022"),
+    description:"अटलबिहारी वाजपेयी यांचं सरकार गेलं होतं. मनमोहनसिंग पंतप्रधान बनले होते. वयोमानामुळे वाजपेयीनी राजकारणातून संन्यास घेतल्यात जमा होती. त्यांचे वारस अडवाणी होते पण त्यानंतरचा नेता कोण असणार यात दुसऱ्या फळीमध्ये बरीच स्पर्धा होती. यात प्रमोद महाजन, सुषमा स्वराज, अरुण जेटली, नितीन गडकरी आघाडीवर होतेच. दरम्यान अजून एक नावही त्यावेळी नेहमी चर्चेत असायचं आणि या एका माणसामुळे नरेंद्र मोदी आणि नितीन गडकरी यांच्यात एका माणसामुळे जोरात पंगा झाला होता.. त्याचाच हा किस्सा. ",   
+  },
+
+  {
+    _id: "WafKlTKUFyw",
+    title: "Masterclass: Myanmar में Narendra Modi की मुश्किल का China ने क्या फायदा उठा लिया? Duniyadari E628",
+    category:"News",
+    channelProfile:"ytc/AMLnZu95miw-xxKObNVp2GD5DlZrteukt1_12cDtOOVqHBQ=s68-c-k-c0x00ffffff-no-rj",
+    views:"89K",
+    channel:"The Lallantop",
+    thumbnail:  "https://i.ytimg.com/vi/WafKlTKUFyw/hqdefault.jpg",
+    uploadedAt:new Date("Jul 30, 2022"),
+    description:"In this episode, Saurabh Dwivedi discusses the Military Junta of Myanmar and its impact on India. Myanmar army did a military coup in the early hours of February 1st, 2021. The army arrested elected leaders like Aung Sang Suu Kyi and others and then announced a nationwide emergency. Despite the restrictions and violence, People continued to protest and kept criticising the Army. Thousands have been locked in jail and many killed in the broad daylight. Recently, Four democracy activists were hanged for their anti-military demonstrations. One of them was a former parliamentarian. He was a close ally of Suu Kyi. Capital punishment is legal yet rare in Myanmar. The previous capital punishment did happen some three decades before.",   
+  },
+
+  {
+    _id: "2WJL19wDH68",
+    title: "JS Engine EXPOSED 🔥 Google's V8 Architecture 🚀 | Namaste JavaScript Ep. 16",
+    category:"Javascript",
+    views:"174K",
+    channel:"Akshay Saini",
+    channelProfile:"ytc/AMLnZu-cSoO0e2Shro4sYkaKYCumlgllLCTYYNf7bk7G_g=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/2WJL19wDH68/hqdefault.jpg",
+    uploadedAt:new Date("Jan 10, 2021"),
+    description:"In-depth explanation of JS Engine Architecture, JavaScript Runtime Environment, JIT compilation, Syntax Parsers, Garbage Collector, and how things work behind the scenes in Google's V8 JS Engine and its architecture.",   
+  },
+
+  {
+    _id: "8zKuNo4ay8E",
+    title: "Asynchronous JavaScript & EVENT LOOP from scratch 🔥 | Namaste JavaScript Ep.15",
+    category:"Javascript",
+    views:"393K",
+    channel:"Akshay Saini",
+    channelProfile:"ytc/AMLnZu-cSoO0e2Shro4sYkaKYCumlgllLCTYYNf7bk7G_g=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/8zKuNo4ay8E/hqdefault.jpg",
+    uploadedAt:new Date("Dec 27, 2020"),
+    description:"In-depth explanation of Event Loop, Web APIs, MicroTask Callback Queue, and how JavaScript works asynchronously in the web browser. Understand how setTimeout works behind the scenes along with fetch and event listeners. ",   
+  },
+
+  {
+    _id: "nqsPmuicJJc",
+    title: "TRUST ISSUES with setTimeout() | Namaste JavaScript Ep.17",
+    category:"Javascript",
+    views:"117K",
+    channel:"Akshay Saini",
+    channelProfile:"ytc/AMLnZu-cSoO0e2Shro4sYkaKYCumlgllLCTYYNf7bk7G_g=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/nqsPmuicJJc/hqdefault.jpg",
+    uploadedAt:new Date("Jan 18, 2021"),
+    description:" This BONUS video covers the fundamentals of setTimeout, how it works behind the scenes. We also discover that setTimeout does not guarantee that the callback method will be executed exactly after a certain delay. Hence, there are trust issues with setTimeout. 😅",   
+  },
+
+  {
+    _id: "zdp0zrpKzIE",
+    title: "map, filter & reduce 🙏 Namaste JavaScript Ep. 19 🔥",
+    category:"Javascript",
+    views:"359K",
+    channel:"Akshay Saini",
+    channelProfile:"ytc/AMLnZu-cSoO0e2Shro4sYkaKYCumlgllLCTYYNf7bk7G_g=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:  "https://i.ytimg.com/vi/zdp0zrpKzIE/hqdefault.jpg",
+    uploadedAt:new Date("Jan 21, 2021"),
+    description:"Map, filter & reduce Array functions are the most popular Higher-Order Functions in JavaScript.  This episode covers map(), filter() & reduce() along with 11 detailed code examples from easy to tricky ones(The last example is the Best). You'll also find homework towards the end of this episode. ❤️",   
+  },
+  
 ];
+
+
+
