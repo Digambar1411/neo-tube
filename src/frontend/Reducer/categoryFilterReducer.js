51e389a017bf37e965bf7f09e0e241d022941606
Digambar <deshawaldigambar@gmail.com>
2022-08-06 18:17:22 +0530

    category reducer

---
+++ src/frontend/Reducer/categoryFilterReducer.js
@@ -0,0 +1,12 @@
+const categoryFilterReducer =(state, action)=>{
+    switch(action.type){
+        case "CATEGORY":
+            return {...state, category:action.payload}
+        default: 
+            return state;
+    }
+
+}
+
+export { categoryFilterReducer}
+
