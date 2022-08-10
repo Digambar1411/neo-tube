2e10a4f3bef75552ae28a1ebb1722f0ff4cd11d9
Digambar <deshawaldigambar@gmail.com>
2022-08-10 11:23:45 +0530

    added small horizontal video cardd component

--- src/frontend/components/index.js
+++ src/frontend/components/index.js
@@ -1,7 +1,8 @@
 import { Navbar } from "./navbar/nav"
 import { Footer} from "./footer/footer";
 import { Sidebar} from "./sidebar/sidebar";
-import { SmallVideoCard } from "./smallVideoCard/smallVideoCard"
-import {CategoryContainer} from "./categoryContainer/categoryContainer"
+import { SmallVideoCard } from "./smallVideoCard/smallVideoCard";
+import {CategoryContainer} from "./categoryContainer/categoryContainer";
+import { SmallHorizontalVideoCard } from "./smallHorizontalCard/smallHorizontalCard"
 
-export { Navbar, Sidebar, Footer, SmallVideoCard, CategoryContainer};
+export { Navbar, Sidebar, Footer, SmallVideoCard, CategoryContainer, SmallHorizontalVideoCard};
