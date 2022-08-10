518debda62213e943b3e1fc3707e88da4f69f957
Digambar <deshawaldigambar@gmail.com>
2022-08-10 13:53:31 +0530

    added contexts

--- src/frontend/contexts/index.js
+++ src/frontend/contexts/index.js
@@ -1,5 +1,6 @@
 import { SidebarProvider ,useSidebar} from "./Sidebar-context";
 import { ThemeProvider, useTheme } from "./theme-context";
+import { useVideos, VideosProvider } from "./video-context";
+import { useCategory, CategoryProvider} from "./category-context"
 
-
-export{ SidebarProvider,  useSidebar, ThemeProvider, useTheme}
+export{ SidebarProvider,  useSidebar, ThemeProvider, useTheme, useVideos, useCategory, CategoryProvider, VideosProvider}
