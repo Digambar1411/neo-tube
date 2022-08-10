134484f61226fdfe33a76b6664b78e8c611e7a98
Digambar <deshawaldigambar@gmail.com>
2022-08-10 13:51:53 +0530

    add categoryBar, small & smallHorizontal videoCard

--- src/frontend/components/index.js
+++ src/frontend/components/index.js
@@ -1,5 +1,8 @@
 import { Navbar } from "./navbar/nav"
 import { Footer} from "./footer/footer";
 import { Sidebar} from "./sidebar/sidebar";
+import { SmallVideoCard } from "./smallVideoCard/smallVideoCard";
+import { CategoryBar } from "./categoryBar/categoryBar";
+import { SmallHorizontalVideoCard } from "./smallHorizontalVideoCard/smallHorizontalVideoCard"
 
-export { Navbar, Sidebar, Footer};
+export { Navbar, Sidebar, Footer, SmallVideoCard, CategoryBar, SmallHorizontalVideoCard};
---
+++ src/frontend/components/smallHorizontalVideoCard/smallHorizontalVideoCard.js
@@ -0,0 +1,24 @@
+import Moment from 'react-moment';
+import { useNavigate } from "react-router-dom";
+
+const SmallHorizontalVideoCard =({video})=>{
+    const navigate = useNavigate();
+    return(
+        <>
+            <div className='sm-hz-video-card flex-row' onClick={()=>navigate(`/video-listing/${video._id}`)}>
+                <img className='sm-card-thumbnail' src={`https://img.youtube.com/vi/${video._id}/maxresdefault.jpg`}/>
+                <div className='sm-card-video-details flex-col mg-left-8px'>
+                    <div className="sm-card-title">{video.title}</div>
+                    <div className='sm-card-creator light-font'>{video.creator}</div>
+                    <div className='flex-row'>
+                        <section className='views sm-card-views light-font'>{video.views} views</section>
+                        <section className="mg-left-4px sm-card-uploadedTime light-font"><Moment fromNow >{video.uploadedAt}</Moment></section>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export { SmallHorizontalVideoCard}
