2be5afee28878f4987451533075a0386039793dc
Digambar <deshawaldigambar@gmail.com>
2022-08-10 13:55:23 +0530

    added filterReducer

---
+++ src/frontend/reducers/categoryFilterReducer.js
@@ -0,0 +1,10 @@
+const categoryFilterReducer =(state, action)=>{
+    switch(action.type){
+        case "CATEGORY":
+            return {...state, category:action.payload}
+        default: 
+            return state;
+    }
+
+}
+
