ac3422ab738a06860752ce44498ee442acf6e98a
Digambar <deshawaldigambar@gmail.com>
2022-08-11 22:26:26 +0530

    feat : added user profile page

---
+++ src/frontend/pages/user details/user-profile.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Navbar, Footer } from "../../components";
+import { useAuth } from "../../contexts";
+import "./user-profile.css";
+
+const UserProfle =()=>{
+    const { user, logoutHandler}= useAuth();
+
+    return(
+    <>
+        <div className="container">
+        <Navbar />
+       <div className="user-profile-page">
+       {
+            user && 
+            <div className="profile-card flex-col center">
+                <div className="user-card-heading">User Details</div>
+                <p>FirstName : {user.firstName}</p>
+                <p>LastName : {user.lastName}</p>
+                <p>Email : {user.email}</p>
+                <button className="auth-btn logout" onClick={logoutHandler}>Logout</button>
+            </div>
+        }
+       </div>
+        <Footer />
+        </div>
+    </>
+
+    )
+}
+
+export { UserProfle}
