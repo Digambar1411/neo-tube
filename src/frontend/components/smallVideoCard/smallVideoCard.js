e04425024b9acc9e4cd9e9eb0b53e3714d55c816
Digambar <deshawaldigambar@gmail.com>
2022-08-13 23:43:24 +0530

    likes and watch later functionality

--- src/frontend/components/smallVideoCard/smallVideoCard.js
+++ src/frontend/components/smallVideoCard/smallVideoCard.js
@@ -1,19 +1,60 @@
 import Moment from 'react-moment';
 import { useState } from 'react';
 import { useNavigate} from "react-router-dom"
+import { removeFromWatch, addToWatch} from "../../services/"
+import {useWatchLaterVideos,useAuth} from "../../contexts"
 import "./smallVideoCard.css";
 
 const SmallVideoCard =({video})=>{
+    const { _id, title, views, creator, channelProfile, uploadedAt} = video;
+    const { watchLaterVideos,setWatchLaterVideos }= useWatchLaterVideos();
+    const { stateAuth:{token, isLoggedIn}}= useAuth();
+    const navigate = useNavigate();
 
     const [showModal, setShowModal] = useState(false);
 
-    const navigate = useNavigate();
+    const isInWatchLater = watchLaterVideos.find(watchLaterVideo=>watchLaterVideo._id===video._id ? true : false)
 
     const toggleModal=()=>{
         setShowModal((showModal)=>!showModal)
     }
-    
-    const { _id, title, views, creator, channelProfile, uploadedAt, thumbnail} = video;
+
+    const addToWatchLaterVideos = async()=>{
+        if(isLoggedIn){
+            try{
+                const response = await addToWatch(video,token)
+                console.log(response)
+                if(response.status===201){
+                    setWatchLaterVideos(response.data.watchlater)
+                }
+        
+            }catch(error){
+                console.log(error);
+            }
+
+        }else{
+            navigate("/login")
+        }
+        
+    }
+
+    const removeFromWatchLaterVideos = async()=>{
+        if(isLoggedIn){
+            try{
+                const response = await removeFromWatch(video._id, token)
+                if(response.status===200){
+                    setWatchLaterVideos(response.data.watchlater)
+                }
+            }catch(error){
+                console.error(error)
+            }
+
+        }else{
+            navigate("/login")
+        }
+        
+    }
+
     return(
         <>
             <div className="video-card flex-col">
@@ -44,11 +85,27 @@ const SmallVideoCard =({video})=>{
 
                     { showModal && 
                         <div className="modal">
+                             {isInWatchLater ?
+                                <div className="menu-item" onClick={removeFromWatchLaterVideos}>
+                                    <span className="material-icons md-28">
+                                        watch_later
+                                    </span>
+                                    <div>Remove from Watch Later</div>
+                                </div>
+                                :
+                                <div className="menu-item" onClick={addToWatchLaterVideos}>
+                                    <span className="material-icons-outlined md-28">
+                                        watch_later
+                                    </span>
+                                    <div>Save to Watch Later</div>
+                                </div>
+                            }
+
                             <div className="menu-item">
                                 <span className="material-icons-outlined md-28">
-                                    watch_later
+                                    playlist_play
                                 </span>
-                                <div>Save to watch later</div>
+                                <div>Add to playlist</div>
                             </div>
                         </div>  
                     }
